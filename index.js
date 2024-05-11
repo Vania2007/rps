291aff68fbf28e8398a2a35b49ae1685d546c9bf
Vania2007 <momotis2007@gmail.com>
2024-05-11 13:11:11 +0300

    add socets

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
-const http = require('http');
-const path = require('path');
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
 
 const server = http.createServer(app);
+const io = new Server(server);
+app.use(express.static(path.join(__dirname, "client")));
 
-app.use(express.static(path.join(__dirname, 'client')));
-
-app.get('/healthcheck', (req, res) => {
-    res.send('<h1>RPS App running...</h1>');
+app.get("/healthcheck", (req, res) => {
+  res.send("<h1>RPS App running...</h1>");
 });
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/index.html");
+});
+io.on("connection", (socket) => {
+  console.log("a user is connected");
 });
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
