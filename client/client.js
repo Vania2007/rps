69700300f605d6408fc278605d64c61924b8f3be
Vania2007 <momotis2007@gmail.com>
2024-05-11 13:31:12 +0300

    socket.on createGame

--- client/client.js
+++ client/client.js
@@ -1,2 +1,16 @@
-console.log('client.js loaded');
+console.log("client.js loaded");
 const socket = io();
+function createGame() {
+  socket.emit("createGame");
+}
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters[Math.floor(Math.random() * charactersLength)];
+  }
+  return result;
+}
+
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { Server } = require("socket.io");
 
 const server = http.createServer(app);
 const io = new Server(server);
+const rooms = {};
 app.use(express.static(path.join(__dirname, "client")));
 
 app.get("/healthcheck", (req, res) => {
@@ -16,6 +17,16 @@ app.get("/", (req, res) => {
 });
 io.on("connection", (socket) => {
   console.log("a user is connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("createGame", () => {
+    const roomUniqueId = makeid(6);
+    console.log(roomUniqueId);
+    // rooms[roomUniqueId] = {};
+    // socket.join(roomUniqueId);
+    // socket.emit("newGame", { roomUniqueId: roomUniqueId });
+  });
 });
 
 server.listen(3000, () => {
