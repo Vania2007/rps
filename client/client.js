b8e7a49697cad6b6668d253c0ebbdca5902f41d3
Vania2007 <momotis2007@gmail.com>
2024-05-12 13:29:11 +0300

    Game is finished

--- client/client.js
+++ client/client.js
@@ -1,7 +1,9 @@
 console.log("client.js loaded");
 const socket = io();
 let roomUniqueId = null;
+let player1 = false;
 function сreateGame() {
+  player1 = true;
   socket.emit("createGame");
 }
 function joinGame() {
@@ -37,3 +39,56 @@ socket.on("playersConnected", (data) => {
   document.getElementById("waitingArea").style.display = "none";
   document.getElementById("gameArea").style.display = "block";
 });
+function sendChoice(rpsValue) {
+  const choiceEvent = player1 ? "p1Choice" : "p2Choice";
+  socket.emit(choiceEvent, {
+    rpsValue: rpsValue,
+    roomUniqueId: roomUniqueId,
+  });
+  let playerChoiceButton = document.createElement("button");
+  playerChoiceButton.style.display = "block";
+  playerChoiceButton.classList.add(rpsValue.toString().toLowerCase());
+  playerChoiceButton.innerText = rpsValue;
+  document.getElementById("player1Choice").innerHTML = "";
+  document.getElementById("player1Choice").appendChild(playerChoiceButton);
+}
+socket.on("p1Choice", (data) => {
+  if (!player1) {
+    createOpponentChoiceButton(data);
+  }
+});
+
+socket.on("p2Choice", (data) => {
+  if (player1) {
+    createOpponentChoiceButton(data);
+  }
+});
+socket.on("result", (data) => {
+  let winnerText = "";
+  if (data.winner != "d") {
+    if (data.winner == "p1" && player1) {
+      winnerText = "You win";
+    } else if (data.winner == "p1") {
+      winnerText = "You lose";
+    } else if (data.winner == "p2" && !player1) {
+      winnerText = "You win";
+    } else if (data.winner == "p2") {
+      winnerText = "You lose";
+    }
+  } else {
+    winnerText = `It's a draw`;
+  }
+  document.getElementById("opponentState").style.display = "none";
+  document.getElementById("opponentButton").style.display = "block";
+  document.getElementById("winnerArea").innerHTML = winnerText;
+});
+
+function createOpponentChoiceButton(data) {
+  document.getElementById("opponentState").innerHTML = "Opponent made a choice";
+  let opponentButton = document.createElement("button");
+  opponentButton.id = "opponentButton";
+  opponentButton.classList.add(data.rpsValue.toString().toLowerCase());
+  opponentButton.style.display = "none";
+  opponentButton.innerText = data.rpsValue;
+  document.getElementById("player2Choice").appendChild(opponentButton);
+}
