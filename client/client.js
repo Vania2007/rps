11c2908556e5b6cd2bb2b163b813e69f4bd0ec33
Vania2007 <momotis2007@gmail.com>
2024-05-12 11:57:15 +0300

    key generation recovery

--- client/client.js
+++ client/client.js
@@ -1,16 +1,6 @@
 console.log("client.js loaded");
 const socket = io();
-function createGame() {
+
+function сreateGame() {
   socket.emit("createGame");
 }
-function makeid(length) {
-  var result = "";
-  var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters[Math.floor(Math.random() * charactersLength)];
-  }
-  return result;
-}
-
--- index.js
+++ index.js
@@ -1,34 +1,50 @@
 const express = require("express");
-const app = express();
 const http = require("http");
 const path = require("path");
 const { Server } = require("socket.io");
 
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const rooms = {};
+
+const room = {};
+
 app.use(express.static(path.join(__dirname, "client")));
 
 app.get("/healthcheck", (req, res) => {
-  res.send("<h1>RPS App running...</h1>");
+  res.send("<h2>RPS App running...</h2>");
 });
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/index.html");
 });
+
 io.on("connection", (socket) => {
   console.log("a user is connected");
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+
   socket.on("createGame", () => {
-    const roomUniqueId = makeid(6);
+    const roomUniqueId = makeid(12);
     console.log(roomUniqueId);
-    // rooms[roomUniqueId] = {};
-    // socket.join(roomUniqueId);
-    // socket.emit("newGame", { roomUniqueId: roomUniqueId });
+    /* rooms[roomUniqueId] = {};
+     socket.join(roomUniqueId);
+     socket.emit("newGame", { roomUniqueId: roomUniqueId }); */
   });
 });
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
+
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters[Math.floor(Math.random() * charactersLength)];
+  }
+  return result;
+}
