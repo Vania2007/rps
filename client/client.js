3c5900eef8d0b99d4e1f1e8beedf2319cf307908
Vania2007 <momotis2007@gmail.com>
2024-05-12 12:41:10 +0300

    add navigator.clipboard

--- client/client.js
+++ client/client.js
@@ -5,10 +5,30 @@ function сreateGame() {
   socket.emit("createGame");
 }
 function joinGame() {
-  roomUniqueId = document.getElementById('roomUniqueId').value;
-  console.log(roomUniqueId)
-  socket.emit('joinGame', {roomUniqueId: roomUniqueId});
+  roomUniqueId = document.getElementById("roomUniqueId").value;
+  console.log(roomUniqueId);
+  socket.emit("joinGame", { roomUniqueId: roomUniqueId });
 }
 socket.on("newGame", (data) => {
+  console.log("newGame");
   roomUniqueId = data.roomUniqueId;
+  document.getElementById("initial").style.display = "none";
+  document.getElementById("gamePlay").style.display = "block";
+  let copyButton = document.createElement("button");
+  copyButton.style.display = "block";
+  copyButton.innerText = "Copy Code";
+  copyButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(roomUniqueId).then(
+      function () {
+        console.log("Async: Copying to clipboard was successful!");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  });
+  document.getElementById(
+    "waitingArea"
+  ).innerHTML = `Waiting for opponent, please share code ${roomUniqueId} to join`;
+  document.getElementById("waitingArea").appendChild(copyButton);
 });
